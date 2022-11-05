90072097b751552fea02c0ef64d6ca9a2ce4d38f
Medzhit Saitov <saitovmedjit@gmail.com>
2022-11-05 15:59:32 +0200

    Added ElegatnOTA update to main class.

--- esp32LedStripClock.ino
+++ esp32LedStripClock.ino
@@ -1,10 +1,44 @@
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncElegantOTA.h>
+
+const char* ssid = "Black Shadow 2.4GHz";
+const char* password = "123454321";
+
+AsyncWebServer server(80);
 
 void setup()
 {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(200, "text/html", "<h1>Hi! I am ESP32.</br>GOTO: <a href=\"/update\">/update</a> for OTA</h1>");
+  });
+
+  AsyncElegantOTA.begin(&server);    // Start ElegantOTA
+  server.begin();
+  Serial.println("HTTP server started");
 
 }
 
 void loop()
 {
-  
+
 }
